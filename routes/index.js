9de61b3b18d41f42f62c1103adbbee92d4a07a48
Craig Chang <craigchang@gmail.com>
2016-08-12 10:35:59 -0700

    Put template 2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET template 2  */
+router.get('/overflow', function(req, res, next) {
+  res.render('overflow/index', {
+    layout: 'overflow/layout', 
+    title: 'Express' 
+  });
+});
+
 module.exports = router;
