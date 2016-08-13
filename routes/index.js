d1885e4fcd7ce428a27ae24c8b6dedce4878e4cd
Arthur Fung <afung@linkedin.com>
2016-08-13 15:40:59 -0700

    Route fixes for images

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var photos = require('../data/photos.json');
+var imageDataDescription = require('../data/imagedescription.json');
+var s3Path = 'https://s3-us-west-2.amazonaws.com/wedding-reg/';
+var s3ThumbnailPrefix = 'thumbnail-';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,31 +21,21 @@ Number.prototype.pad = function(size) {
   return s;
 }
 var imageSet = [1, 2, 3, 4, 5, 8, 10, 11, 13, 14, 15];
-var imageDataDescription = [
-  { title: 'Diam tempus accumsan', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
-  { title: 'Vivamus convallis libero', description: 'Sed velit lacus, laoreet at venenatis convallis in lorem tincidunt.' },
-  { title: 'Nec accumsan enim felis', description: 'Maecenas eleifend tellus ut turpis eleifend, vitae pretium faucibus.' },
-  { title: 'Donec maximus nisi eget', description: 'Tristique in nulla vel congue. Sed sociis natoque parturient nascetur.' },
-  { title: 'Nullam vitae nunc vulputate', description: 'In pellentesque cursus velit id posuere. Donec vehicula nulla.' },
-  { title: 'Phasellus magna faucibus', description: 'Nulla dignissim libero maximus tellus varius dictum ut posuere magna.' },
-  { title: 'Proin quis mauris', description: 'Etiam ultricies, lorem quis efficitur porttitor, facilisis ante orci urna.' },
-  { title: 'Gravida quis varius enim', description: 'unc egestas congue lorem. Nullam dictum placerat ex sapien tortor mattis.' },
-  { title: 'Morbi eget vitae adipiscing', description: 'In quis vulputate dui. Maecenas metus elit, dictum praesent lacinia lacus.' },
-  { title: 'Habitant tristique senectus', description: 'Vestibulum ante ipsum primis in faucibus orci luctus ac tincidunt dolor.' },
-  { title: 'Pharetra ex non faucibus', description: 'Ut sed magna euismod leo laoreet congue. Fusce congue enim ultricies.' }
-
-];
 var imagesData = [];
 var i = 0;
+var photosRows4 = [];
+
 for (i = 0; i < imageSet.length; i++) { 
   temp = {
-    full: 'https://s3-us-west-2.amazonaws.com/wedding-reg/' + imageSet[i].pad(4) + '.jpg',
-    thumb: 'https://s3-us-west-2.amazonaws.com/wedding-reg/thumbnail-' + imageSet[i].pad(2) + '.jpg'
+    full: s3Path + imageSet[i].pad(4) + '.jpg',
+    thumb: s3Path + s3ThumbnailPrefix + imageSet[i].pad(2) + '.jpg'
   };
-  temp.title = imageDataDescription[i].title;
-  temp.description = imageDataDescription[i].description;
+  temp.title = imageDataDescription['imageDecsripton'][i].title;
+  temp.description = imageDataDescription['imageDecsripton'][i].description;
   imagesData.push(temp);
 }
+while(photos[0])
+  photosRows4.push(photos.splice(0,4));
 
 /* GET template 1  */
 router.get('/lens', function(req, res, next) {
@@ -55,10 +48,6 @@ router.get('/lens', function(req, res, next) {
     }
   });
 });
-
-var photosRows4 = [];
-while(photos[0])
-  photosRows4.push(photos.splice(0,4));
 
 /* GET template 2  */
 router.get('/overflow', function(req, res, next) {
