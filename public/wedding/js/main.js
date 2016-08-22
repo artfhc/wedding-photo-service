5722c683198697e15f5f8a7fef4ae354dd9a60cc
Craig Chang <craigchang@gmail.com>
2016-08-21 22:44:19 -0700

    add in i18n

--- public/wedding/js/main.js
+++ public/wedding/js/main.js
@@ -56,3 +56,17 @@ google.maps.event.addDomListener(window, 'load', function() {
     usePopupNav: true
   });
 }(jQuery));
+
+// Language
+(function($) {
+  $('select[name="language-select"]').change(function(ev) {
+    var $this = $(this);
+    if($this.val() == 'en')
+      document.cookie = 'locale=en_US';
+    else if ($this.val() == 'zh')
+      document.cookie = 'locale=zh_CN';
+    else
+      return false;
+    window.location.href = window.location.href;
+  });
+}(jQuery));
